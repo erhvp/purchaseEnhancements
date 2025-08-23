624226443cf47b187c9d5120790c5646e92b674a
Harsh Pandey <harshvardhanpandeycse@gmail.com>
2025-08-24 00:00:04 +0530

    Update notification_recipient_rule.py

--- purchase_enhancements/purchase_enhancements/doctype/notification_recipient_rule/notification_recipient_rule.py
+++ purchase_enhancements/purchase_enhancements/doctype/notification_recipient_rule/notification_recipient_rule.py
@@ -1,5 +1,7 @@
 # delivery_reminder.py
-import frappe
+from frappe.model.document import Document
 
+class NotificationRecipientRule(Document):
+    pass
 # You can leave it empty if there is no custom logic
 
