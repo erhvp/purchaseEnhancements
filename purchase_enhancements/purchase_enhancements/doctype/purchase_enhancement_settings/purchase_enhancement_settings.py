381c8fb3c791ce3ed7355fe31914b42bbafb6b72
Harsh Pandey <harshvardhanpandeycse@gmail.com>
2025-08-23 23:58:39 +0530

    Update purchase_enhancement_settings.py

--- purchase_enhancements/purchase_enhancements/doctype/purchase_enhancement_settings/purchase_enhancement_settings.py
+++ purchase_enhancements/purchase_enhancements/doctype/purchase_enhancement_settings/purchase_enhancement_settings.py
@@ -1,5 +1,7 @@
 # delivery_reminder.py
-import frappe
+from frappe.model.document import Document
 
+class PurchaseEnhancementSettings(Document):
+    pass
 # You can leave it empty if there is no custom logic
 
